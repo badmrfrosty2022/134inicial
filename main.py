30beea433920e75df27085c7e7c47c9593d091ea
eduardo <ers_eduardo@hotmail.com>
2022-05-06 18:23:51 -0300

    Aula de teste de unidade

--- main.py
+++ main.py
@@ -6,20 +6,30 @@ def imprimir_oi(nome):
 def somar(numero_a, numero_b):
     return numero_a + numero_b
 
+
+def subtrair(numero_a, numero_b):
+    return numero_a - numero_b
+
+
+def multiplicar(numero_a, numero_b):
+    return numero_a * numero_b
+
+
+def dividir(numero_a, numero_b):
+    try:
+        return numero_a / numero_b
+    except ZeroDivisionError:
+        return 'Não dividiras por zero'
+
+
 if __name__ == '__main__':
     imprimir_oi('Edu')
 
+    # Chamar a definição somar e mostrar o resultado
     resultado = somar(5, 7)
     print (f'A soma: {resultado}')
 
-def teste_somar():
-    #1 - Configura
-    numero_a = 8
-    numero_b = 7
-    resultado_esperado = 15
+    # Chamar a definição subtrair e mostrar o resultado
+    resultado = subtrair(25, 8)
+    print (f'A subtração: {resultado}')
 
-    #2 - Executa
-    resultado_obtido = somar(numero_a, numero_b)
-
-    #3 - Valida
-    assert resultado_obtido == resultado_esperado
