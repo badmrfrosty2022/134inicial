368f4583fd605eb738c271eab0557b3e9f02de6d
eduardo <ers_eduardo@hotmail.com>
2022-06-23 20:01:04 -0300

    Enderecamento de arquivo compativel com diversos sistemas operacionais. Encadeamento de 2 chamadas de API

--- tests/api/test_store.py
+++ tests/api/test_store.py
@@ -1,9 +1,10 @@
-# ToDO: 3 - criar uma venda de um pet para um usuário
 # ToDo: 4 - consultar os dados do pet que foi vendido
+import os
+
 import requests
 import json
 
-url= 'https://petstore.swagger.io/v2/store/order'
+url = 'https://petstore.swagger.io/v2/'
 headers = {'Content-Type': 'application/json'}
 
 
@@ -19,12 +20,14 @@ def teste_vender_pet():
     status_pedido_esperado = 'placed'
 
     # Executa
-    resultado_obtido = requests.post(
-        url=url,
-        headers=headers,
-        data=open('//Users//eduardo//PycharmProjects//134inicial//vendors//json//pedido1.json')
-    )
+    caminho = os.path.abspath(__file__ + "/../../../") + os.sep + 'vendors' + os.sep + 'json' + os.sep
+    # print(f'caminho1 = {caminho}')
 
+    resultado_obtido = requests.post(
+        url=url + 'store/order',
+        headers=headers,
+        data=open(caminho + 'pedido1.json')
+    )
 
     # Valida
     print(resultado_obtido)
@@ -36,3 +39,27 @@ def teste_vender_pet():
     assert corpo_do_resultado_obtido['petId'] == pet_id_esperado
     assert corpo_do_resultado_obtido['status'] == status_pedido_esperado
 
+    # Extrai
+    pet_id_extraido = corpo_do_resultado_obtido.get('petId')
+
+    # Realizar a 2ª Transação
+
+    # Configura
+    # Dados de Entrada
+    # Extraidos da 1ª transação acima
+
+    # Resultado Esperado
+    pet_name_esperado = 'Snoopy'
+    status_code_esperado = 200
+
+    # Executa
+    resultado_obtido = requests.get(
+        url=url + 'pet/' + str(pet_id_extraido),
+        headers=headers
+    )
+
+    # Valida
+    assert resultado_obtido.status_code == status_code_esperado
+    corpo_do_resultado_obtido = resultado_obtido.json()
+    print(json.dumps(corpo_do_resultado_obtido, indent=4))
+    assert corpo_do_resultado_obtido['name'] == pet_name_esperado
