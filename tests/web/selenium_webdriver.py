250fb4fbc5fcd1980ee65515198f65b5bb5e0774
eduardo <ers_eduardo@hotmail.com>
2022-07-17 14:20:22 -0300

    Automatizado o fluxo na pagina Home

--- tests/web/selenium_webdriver.py
+++ tests/web/selenium_webdriver.py
@@ -1,5 +1,6 @@
 # Configura
 # Bibliotecas / Imports
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -25,6 +26,8 @@ class Testes:
         # instanciar a biblioteca / motor / engine
         # informar onde está o WebDriver (driver do Chrome)
         self.driver = webdriver.Chrome()
+        # espera até 15 segundos por qualquer elemento
+        self.driver.implicitly_wait(15)
 
     # Fim
     def teardown_method(self):
@@ -42,18 +45,21 @@ class Testes:
         lista = self.driver.find_element(By.NAME, "fromPort")
         lista.click()
         # selecionar a cidade de origem desejada
-        lista.find_element(By.XPATH, '//option[ .= "São Paolo"]').click
+        lista.find_element(By.XPATH, f'//option[ .= "{origem}"]').click()
         # clicar na lsita de cidades de destino
         lista = self.driver.find_element(By.NAME, "toPort")
         lista.click()
         # selecionar a cidade de destino desejada
-        lista.find_element(By.XPATH, '//option[ .= "New York"]').click
+        lista.find_element(By.XPATH, f'//option[ .= "{destino}"]').click()
         # clicar no botão de procurar voos
-        self.driver.find_element(By.CSS_SELECTOR, 'input.btn.btn-primary')
+        self.driver.find_element(By.CSS_SELECTOR, 'input.btn.btn-primary').click()
 
 
         # Pagina Lista de passagem
         # Executa / Valida
+        assert self.driver.find_element(By.TAG_NAME, 'h3').text == titulo_passagens_esperado
+        self.driver.find_element(By.CSS_SELECTOR, "tr:nth-child(1) .btn").click
+        time.sleep(3)
 
         # Pagina de compra
         # Executa / Valida
